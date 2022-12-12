a32df68a4f338669089ef66bc3b9dbb7aa8c13c1
Manu Chaudhary <manu@imagekit.io>
2022-12-12 16:25:06 +0530

    handle filesystem files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import StorageBase from 'ghost-storage-base'
 import got from 'got'
 import ImageKit from 'imagekit'
 import { join, parse } from 'path'
+import serveStatic from 'serve-static'
+const fileServe = serveStatic(join(process.env.GHOST_CONTENT as string, "images"), {
+    fallthrough: true,
+    maxAge: "1y"
+});
 
 const readFileAsync = (fp: string): Promise<Buffer> => new Promise((resolve, reject) => readFile(fp, (err, data) => err ? reject(err) : resolve(data)))
 const stripLeadingSlash = (s: string) => s.indexOf('/') === 0 ? s.substring(1) : s
@@ -65,6 +70,7 @@ class Store extends StorageBase {
     }
 
     async exists(fileName: string, targetDir?: string): Promise<boolean> {
+        console.log("exists", fileName, targetDir)
         const filePath = stripLeadingSlash(join(targetDir || "", fileName));
         const response = await got(new URL(filePath, this.urlEndpoint), {
             responseType: 'buffer',
@@ -75,6 +81,7 @@ class Store extends StorageBase {
     }
 
     async save(image: StorageBase.Image, targetDir?: string): Promise<string> {
+        console.log("save", image.name)
         const folder = targetDir || this.getTargetDir(this.uploadOptions.folder)
         const fileName = this.getSanitizedFileName(image.name);
 
@@ -94,16 +101,22 @@ class Store extends StorageBase {
     }
 
     serve() {
-        return (req: Express.Request, res: Express.Response, next: () => void) => {
-            next();
+        return (req: any, res: any, next: any) => {
+            console.log("serve", req.url)
+            fileServe(req, res, () => {
+                console.log("file not found")
+                res.status(404).end();
+            });
         };
     }
 
     async delete(fileName: string, targetDir?: string): Promise<boolean> {
+        console.log("delete", fileName, targetDir)
         throw new Error("Currently deleting a file by path is not possible via API.")
     }
 
     async read(options?: StorageBase.ReadOptions): Promise<Buffer> {
+        console.log("Read", options)
         return await got(new URL(options?.path || "", this.urlEndpoint), {
             responseType: 'buffer',
             resolveBodyOnly: true
